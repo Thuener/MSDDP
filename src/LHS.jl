ba62c5f53249eebd2c171d1d65f1571a5684d429
Thuener Silva <thuener@gmail.com>
2016-05-23 13:48:54 -0300

    Bug fix lhs number of samples

--- src/LHS.jl
+++ src/LHS.jl
@@ -4,14 +4,14 @@ export lhsnorm
 using Distributions
 
 # Latin hypercube sampling for normal distribution
-function lhsnorm(μ,Σ,n; rando=true)
+function lhsnorm(μ::Array{Float64,1}, Σ::Array{Float64,2}, n::Int64; rando=true)
   z = rand(MvNormal(μ,Σ),n)'
 
   p = length(μ)
   x = zeros(size(z))
   for i=1:p
     idx = sortperm(z[:,i])
-    x[idx,i] = collect(1:10)
+    x[idx,i] = collect(1:n)
   end
 
   if rando
