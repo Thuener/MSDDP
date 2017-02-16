0b74e0e16d16450d7d05354936f653c982c5d4c5
Thuener Silva <thuener@gmail.com>
2017-02-16 08:28:50 -0200

    Change period and add option to plot single graph

--- src/exp/plot/effron.jl
+++ src/exp/plot/effron.jl
@@ -1,22 +1,31 @@
 using DataFrames, Gadfly
-T = 12
-table = readcsv("5FF_BM100_Large_$(T)_table.csv") #TODO colocar como parametro
+
+println(ARGS)
+file = ARGS[1] # Name of the file to be processed
+single = parse(Bool,ARGS[2]) # if it is a single efficient frontier (true or false)
+
+table = readcsv("$(file)")
 df2 = DataFrame()
 
 df2[Symbol("Trans. cost")] = vec(vcat(table[:,1],table[:,1],table[:,1]))
 df2[:γ] = vec(vcat(table[:,2],table[:,2],table[:,2]))
 df2[:OF] = vec(vcat(table[:,3],table[:,4],table[:,5]))
 
-N = 10 # Numer of lines on table.csv #TODO colocar como parametro
+N = size(table,1) # Numer of lines on table.csv
 df2[:type] = vcat(fill("MSDDP",N),fill("One-Step",N),fill("One-Step M.",N))
-df2[:M] = vec(vcat((table[:,3]+1).^(1/T) -1,(table[:,4]+1).^(1/T) -1,(table[:,5]+1).^(1/T) -1))
+df2[:M] = vec(vcat((table[:,3]),(table[:,4]),(table[:,5])))
 
-df2[:Mmin] = vec(vcat((table[:,6]+1).^(1/T) -1,(table[:,7]+1).^(1/T) -1,(table[:,8]+1).^(1/T) -1))
-df2[:Mmax] = vec(vcat((table[:,9]+1).^(1/T) -1,(table[:,10]+1).^(1/T) -1,(table[:,11]+1).^(1/T) -1))
+df2[:Mmin] = vec(vcat((table[:,6]),(table[:,7]),(table[:,8])))
+df2[:Mmax] = vec(vcat((table[:,9]),(table[:,10]),(table[:,11])))
 
-#coord = Coord.cartesian(ymin=0.000, ymax=0.015)
-
-myplot =  plot(df2,ygroup="type",x="γ",y="M", ymin="Mmin", ymax="Mmax",color="Trans. cost",Geom.subplot_grid(Geom.line,Geom.errorbar)
-          ,Scale.color_discrete(),Guide.ylabel("Monthly Expected Return"),Theme(line_width=2px,grid_line_width=2px))
-
-draw(SVG("myplot3.svg", 9inch, 9inch), myplot) # paper
+myplot =0
+if !single
+  coord = Coord.cartesian(ymin=0.000, ymax=0.151)
+  myplot =  plot(df2,ygroup="type",x="γ",y="M", ymin="Mmin", ymax="Mmax",color="Trans. cost",Geom.subplot_grid(coord,Geom.line,Geom.errorbar)
+          ,Scale.color_discrete(),Guide.ylabel("Expected return (in 12 months)"),Theme(line_width=2px,grid_line_width=2px))
+  draw(SVG("effron.svg", 9inch, 9inch), myplot) # paper
+else
+  myplot =  plot(df2,x="γ",y="M", ymin="Mmin", ymax="Mmax",color="type",Guide.colorkey(""),Geom.line,Geom.errorbar
+            ,Scale.color_discrete(),Guide.ylabel("Expected return (in 12 months)"),Theme(line_width=2px,grid_line_width=2px))
+  draw(SVG("singleeffron.svg", 9inch, 6inch), myplot) # paper
+end
