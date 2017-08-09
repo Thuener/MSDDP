daafee52146fc3ebb421c8ce59f28661cd763ba3
Thuener Silva <thuener@gmail.com>
2017-08-09 10:14:26 -0300

    Adjust test sets

--- test/inputs.jl
+++ test/inputs.jl
@@ -1,104 +1,117 @@
 using HMM_MSDDP
-using Distributions
-using Base.Test
+using Distributions, Base.Test, CPLEX
 
-info("Test with specific base")
-srand(123)
-N = 5
-T = 10
-K = 1
-S = 100
-α = 0.95
-W_ini = 1.0
-x_ini = zeros(N)
-x0_ini = W_ini
-c = 0.00
-M = 9999999
-γ = 0.1
-S_LB = 1000
-S_LB_inc = 100
-S_FB = 1
-GAPP = 1
-Max_It = 100
-α_lB = 0.9
+@testset "Inputs" begin
+    info("Test with specific base")
+    srand(123)
+    N = 5
+    T = 10
+    K = 1
+    S = 100
+    α = 0.95
+    W_ini = 1.0
+    x_ini = zeros(N)
+    x0_ini = W_ini
+    c = 0.00
+    maxvl = 9999999
+    γ = 0.1
+    samplower = 1000
+    samplower_inc = 100
+    nit_before_lower = 1
+    gap = 1.
+    max_it = 100
+    α_lower = 0.9
 
-dH  = MSDDPData( N, T, K, S, α, x_ini, x0_ini, W_ini, c, M, γ,
-                S_LB, S_LB_inc, S_FB, GAPP, Max_It, α_lB )
+    ms = ModelSizes(T, N, K, S)
 
-#Run the code to output HMM/LHS data
-file = "./test_inputs.csv"
-ret = readcsv(file, Float64)
+    #Run the code to output HMM/LHS data
+    file = "./test_inputs.csv"
+    ret = readcsv(file, Float64)
 
-dM, m = inithmm(ret, dH)
+    mk, hmm = inithmm(ms, ret)
+
+    m = MSDDPModel(MAAParameters(α, γ, c, x_ini, x0_ini, maxvl),
+        SDDPParameters(max_it, samplower, samplower_inc, nit_before_lower, gap, α_lower),
+        mk;
+        lpsolver = CplexSolver(CPX_PARAM_SCRIND=0, CPX_PARAM_LPMETHOD=2))
 
 
-LB, UB = sddp(dH, dM)
-@test_approx_eq_eps mean(LB) 1.0943571 1e-4
-@test_approx_eq_eps UB       1.0940507 1e-4
+    LB, UB = solve(m, m.param)
+    @test isapprox( mean(LB), 1.0943571; atol= 1e-4)
+    @test isapprox( UB      , 1.0940507; atol= 1e-4)
 
-# Changing some parameters
-info("Test with same base changing some parameters")
-dH.γ = 0.01
-LB, UB = sddp(dH, dM)
-@test_approx_eq_eps mean(LB) 1.012397 1e-4
-@test_approx_eq_eps UB       1.012393 1e-4
+    # Changing some parameters
+    info("Test with same base changing some parameters")
+    setγ!(m, 0.01)
+    reset!(m)
+    LB, UB = solve(m, m.param)
+    @test isapprox( mean(LB), 1.012397; atol= 1e-4)
+    @test isapprox( UB      , 1.012393; atol= 1e-4)
 
-dH.α = 0.90
-LB, UB = sddp(dH, dM)
-@test_approx_eq_eps mean(LB) 1.014320 1e-4
-@test_approx_eq_eps UB       1.014329 1e-4
+    setα!(m, 0.90)
+    reset!(m)
+    LB, UB = solve(m, m.param)
+    @test isapprox( mean(LB), 1.014320; atol= 1e-4)
+    @test isapprox( UB      , 1.014329; atol= 1e-4)
 
-dH.T = 2
-LB, UB = sddp(dH, dM)
-@test_approx_eq_eps mean(LB) 1.0015821 1e-4
-@test_approx_eq_eps UB       1.0015821 1e-4
+    setnstages!(m, 2)
+    reset!(m)
+    LB, UB = solve(m, m.param)
+    @test isapprox( mean(LB), 1.0015821; atol= 1e-4)
+    @test isapprox( UB      , 1.0015821; atol= 1e-4)
 
-# Changing the number of states
-dH.K = 3
-dM, m = inithmm(ret, dH)
+    # Changing the number of states
+    setnstates!(m, 3)
+    mk, hmm = inithmm(m.sizes, ret)
 
-order = Array(Int64,dH.K)
-found = true
-mm = mean(m[:means_],2)
-for k = 1:dH.K
-  if abs(mm[k] - 0.007) < 1e-3
-    order[1] = k
-  elseif abs(mm[k] + 0.012) < 1e-3
-      order[2] = k
-    elseif abs(mm[k] - 0.021) < 1e-3
-      order[3] = k
-    else
-      found = false
-  end
+    order = Array(Int64, nstates(m))
+    found = true
+    mm = mean(hmm[:means_],2)
+    for k = 1:nstates(m)
+      if abs(mm[k] - 0.007) < 1e-3
+        order[1] = k
+      elseif abs(mm[k] + 0.012) < 1e-3
+          order[2] = k
+        elseif abs(mm[k] - 0.021) < 1e-3
+          order[3] = k
+        else
+          found = false
+      end
+    end
+    # Has to enter in one if every time
+    @test found == true
+
+    setmarkov!(m, mk)
+    setinistate!(m, order[1])
+    reset!(m)
+    LB, UB = solve(m, m.param)
+    @test isapprox( mean(LB), 1.002671; atol= 1e-4)
+    @test isapprox( UB      , 1.002671; atol= 1e-4)
+
+    setinistate!(m, order[2])
+    reset!(m)
+    LB, UB = solve(m, m.param)
+    @test isapprox( mean(LB), 1; atol= 1e-4)
+    @test isapprox( UB      , 1; atol= 1e-4)
+
+    setinistate!(m, order[3])
+    reset!(m)
+    LB, UB = solve(m, m.param)
+    @test isapprox( mean(LB), 1.0027506; atol= 1e-4)
+    @test isapprox( UB      , 1.0027506; atol= 1e-4)
+
+    setγ!(m, 0.1)
+    setα!(m, 0.95)
+    setnstages!(m, 10)
+    setinistate!(m, order[2])
+    r = zeros(nstages(m), nassets(m), nstates(m), nscen(m))
+    for t = 1:nstages(m)
+      r[t,:,:,:] = mk.ret[1,:,:,:]
+    end
+    mk.ret = r
+
+    reset!(m)
+    LB, UB = solve(m, m.param)
+    @test isapprox( mean(LB), 1.04428 ; atol= 1e-2)
+    @test isapprox( UB      , 1.04243 ; atol= 1e-2)
 end
-# Has to enter in one if every time
-@test found == true
-
-dM.k_ini = order[1]
-LB, UB = sddp(dH, dM)
-@test_approx_eq_eps mean(LB) 1.002671 1e-4
-@test_approx_eq_eps UB       1.002671 1e-4
-
-dM.k_ini = order[2]
-LB, UB = sddp(dH, dM)
-@test_approx_eq_eps mean(LB) 1 1e-4
-@test_approx_eq_eps UB       1 1e-4
-
-dM.k_ini = order[3]
-LB, UB = sddp(dH, dM)
-@test_approx_eq_eps mean(LB) 1.0027506 1e-4
-@test_approx_eq_eps UB       1.0027506 1e-4
-
-dH.γ = 0.1
-dH.α = 0.95
-dH.T = 10
-dM.k_ini = order[2]
-r = zeros(dH.T, dH.N, dH.K, dH.S)
-for t = 1:dH.T
-  r[t,:,:,:] = dM.r[1,:,:,:]
-end
-dM.r = r
-
-LB, UB = sddp(dH, dM)
-@test_approx_eq_eps mean(LB) 1.04428 1e-2
-@test_approx_eq_eps UB       1.04243 1e-2
--- test/sdp.jl
+++ test/sdp.jl
@@ -1,51 +1,53 @@
 using Base.Test
 using SDP, AR
 
-srand(123)
-T_s = 240
-N = 3
-Sc = 1000
-T_l = 120
+@testset "SDP" begin
+    srand(123)
+    T_s = 240
+    N = 3
+    Sc = 1000
+    T_l = 120
 
-# AR
-Σ = [0.002894	0.003532	0.00391	-0.000115; 0.003532	0.004886	0.005712	-0.000144; 0.00391	0.005712	0.007259	-0.000163; -0.000115	-0.000144	-0.000163	0.0529]
-b_r = [ 0.0028; 0.0049; 0.0061]
-b_z = [0.9700]
-a_r  = [0.0053; 0.0067; 0.0072]
-a_z  = [0.0000]
-r_f = 0.00042
+    # AR
+    Σ = [0.002894	0.003532	0.00391	-0.000115; 0.003532	0.004886	0.005712	-0.000144; 0.00391	0.005712	0.007259	-0.000163; -0.000115	-0.000144	-0.000163	0.0529]
+    b_r = [ 0.0028; 0.0049; 0.0061]
+    b_z = [0.9700]
+    a_r  = [0.0053; 0.0067; 0.0072]
+    a_z  = [0.0000]
+    r_f = 0.00042
 
-dF = ARData(a_z, a_r, b_z, b_r, Σ, r_f)
+    dF = ARData(a_z, a_r, b_z, b_r, Σ, r_f)
 
-# Generate the series ρ and z
-se_ = series(dF, Sc, T_s)
-ρ = se_[1:N,:,:]
-rets = exp(ρ)-1 -dF.r_f
-z = reshape(se_[N+1,:,:], T_s, Sc)
+    # Generate the series ρ and z
+    se_ = series(dF, Sc, T_s)
+    ρ = se_[1:N,:,:]
+    rets = exp(ρ)-1 -dF.r_f
+    z = reshape(se_[N+1,:,:], T_s, Sc)
 
 
-# Parameters SDP
-T = 12
-L = 1000
-S = 500
-α = 0.9
-γ = 0.012
+    # Parameters SDP
+    T = 12
+    L = 1000
+    S = 500
+    α = 0.9
+    γ = 0.012
 
-dS = SDPData(N, T, L, S, α, γ)
+    dS = SDPData(N, T, L, S, α, γ)
 
-# Split z
-z_l = splitequaly(dS.L, z)
+    # Split z
+    z_l = splitequaly(dS.L, z)
 
-# Run SDP
-Q_l = backward(dF, dS, z_l)
-ws = zeros(Float64, Sc)
-all = Array(Float64, N+1,T-1, Sc)
-for se = 1:Sc
-  ws[se], all[:,:,se] = forward(dS, dF, Q_l, z_l, vec(z[:,se]), rets[:,:,se])
+    # Run SDP
+    Q_l = backward(dF, dS, z_l)
+    ws = zeros(Float64, Sc)
+    all = Array(Float64, N+1,T-1, Sc)
+    for se = 1:Sc
+      ws[se], all[:,:,se] = forward(dS, dF, Q_l, z_l, vec(z[:,se]), rets[:,:,se])
+    end
+
+
+    @test isapprox( mean(ws), 1.01161; atol= 1e-4)
+    @test isapprox( std(ws), 0.02505; atol= 1e-4)
+    @test isapprox( mean(all[1,:,:]), 0.893047 ; atol= 1e-4)
+    @test isapprox( mean(all[4,:,:]), 0.0378636; atol= 1e-4)
 end
-
-
-@test_approx_eq_eps mean(ws) 1.01161 1e-4
-@test_approx_eq_eps std(ws) 0.02505 1e-4
-@test_approx_eq_eps mean(all[1,:,:]) 0.893047  1e-4
-@test_approx_eq_eps mean(all[4,:,:]) 0.0378636 1e-4
